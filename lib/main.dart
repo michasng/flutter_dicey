4a7627ac186f2f119bf512f852fed1ba9e733c18
Micha Sengotta <micha.sengotta@gmail.com>
2023-11-12 22:56:36 +0100

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const PlayRoute(),
     );
   }
